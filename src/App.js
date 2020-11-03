8fc82b59e41d889c76940d78bf8529374f1b8d30
Anna-Myzukina <anna.muzykina83@gmail.com>
2020-11-03 23:15:35 +0300

    modify pagination

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 function App() {
   const [games, setGames] = useState([])
   const [currentPages, setCurrentPages] = useState(1)
-  const [gamesPerPage] = useState(20)
+  const [gamesPerPage] = useState(10)
 
 
   useEffect(() => {
@@ -18,11 +18,13 @@ function App() {
     }
 
     fetchGames()
+    console.log(games)
   }, [])
 
-  const indexOfLastGame = currentPages * gamesPerPage
-  const indexOfFirstGame = indexOfLastGame - gamesPerPage
-  const currentGames = games.slice(indexOfFirstGame, indexOfLastGame)
+  //to get current games we need:
+  const indexOfLastGame = currentPages * gamesPerPage // index of last game on the ferst page will be 1 * 20 = 20; 2 * 20 = 40...
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage // 20 - 20 = 0; 40 - 20 = 20...
+  const currentGames = games.slice(indexOfFirstGame, indexOfLastGame) // [0, 20]; [20, 40]...
   
 
   console.log(games)
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -15,14 +15,14 @@ function Pagination({ pages = 24, setCurrentPages }) {
 
         //1) if user is on page 1: 1 2 ... 24next
         if (currentNumberOfPage >= 1 && currentNumberOfPage <= 2) {
-            tempNumberOfPages = [1, 2, '...', numberOfpages.length]
+            tempNumberOfPages = [1, 2, '...', numberOfpages.length] //[1,2,'...',24]
         }
 
 
         //2) if user is on page 3: previous 1 2 3 4 ... 24 next
         else if (currentNumberOfPage === 3) {
-            const sliced = numberOfpages.slice(0, 4)
-            tempNumberOfPages = [...sliced, '...', numberOfpages.length]
+            const sliced = numberOfpages.slice(0, 4) //[1,2,3,4]
+            tempNumberOfPages = [...sliced, '...', numberOfpages.length] //[1,2,3,4,'...',24]
         }
 
         //3) from page number 4 till 22: previous 1... 9 10 11 ... 24 next
@@ -44,7 +44,7 @@ function Pagination({ pages = 24, setCurrentPages }) {
     return (
             <div className="pagination-container">
                 <a 
-                href="#"
+                href="!#"
                 className={`${currentNumberOfPage === 1 ? 'display_none' : ''}`}
                 onClick = {() => setCurrentNumberOfPage((previousState) => previousState === 1 ? previousState : previousState - 1)}>
                     previous</a>
@@ -52,7 +52,7 @@ function Pagination({ pages = 24, setCurrentPages }) {
                 {arrOfCurrentPage.map(((page, ind) => {
                     return(
                     <a 
-                    href="#" 
+                    href="!#" 
                     key={ind}
                     className={`${currentNumberOfPage === page ? 'active' : ''}`}
                     onClick={() => setCurrentNumberOfPage(page)}
@@ -64,7 +64,7 @@ function Pagination({ pages = 24, setCurrentPages }) {
                 }))}
 
                 <a 
-                href="#"
+                href="!#"
                 className={`${currentNumberOfPage === numberOfpages.length ? 'display_none' : ''}`}
                 onClick = {() => setCurrentNumberOfPage((previousState) => previousState === numberOfpages.length ? previousState : previousState + 1)}>
                     next</a>
